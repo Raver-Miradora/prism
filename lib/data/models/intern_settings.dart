43df9c7007f9e3d11a0f350103e36fb729359e18
Raver Miradora <ravermiradora0@gmail.com>
2026-03-29 22:40:29 +0800

    Add documentation to InternSettings model

--- lib/data/models/intern_settings.dart
+++ lib/data/models/intern_settings.dart
@@ -1,9 +1,14 @@
+/// Represents the system-wide configuration for an intern, including work
+/// schedule, target hour quota, and program classification.
 class InternSettings {
   final int id;
   final int targetHours;
   final String expectedTimeIn;
   final String expectedTimeOut;
   final int lunchBreakMins;
+  final double? officeLat;
+  final double? officeLng;
+  final String programType; // OJT, SPES, Immersion, GIP
 
   InternSettings({
     this.id = 1, // Singleton row
@@ -11,15 +16,21 @@ class InternSettings {
     required this.expectedTimeIn,
     required this.expectedTimeOut,
     required this.lunchBreakMins,
+    this.officeLat,
+    this.officeLng,
+    this.programType = 'OJT',
   });
 
   factory InternSettings.fromMap(Map<String, dynamic> map) {
     return InternSettings(
       id: map['id'] as int? ?? 1,
       targetHours: map['target_hours'] as int,
-      expectedTimeIn: map['expected_time_in'] as String,
-      expectedTimeOut: map['expected_time_out'] as String,
-      lunchBreakMins: map['lunch_break_mins'] as int,
+      expectedTimeIn: map['expected_time_in'] as String? ?? '08:00',
+      expectedTimeOut: map['expected_time_out'] as String? ?? '17:00',
+      lunchBreakMins: map['lunch_break_mins'] as int? ?? 60,
+      officeLat: map['office_lat'] as double?,
+      officeLng: map['office_lng'] as double?,
+      programType: map['program_type'] as String? ?? 'OJT',
     );
   }
 
@@ -30,6 +41,9 @@ class InternSettings {
       'expected_time_in': expectedTimeIn,
       'expected_time_out': expectedTimeOut,
       'lunch_break_mins': lunchBreakMins,
+      'office_lat': officeLat,
+      'office_lng': officeLng,
+      'program_type': programType,
     };
   }
 
@@ -38,6 +52,9 @@ class InternSettings {
     String? expectedTimeIn,
     String? expectedTimeOut,
     int? lunchBreakMins,
+    double? officeLat,
+    double? officeLng,
+    String? programType,
   }) {
     return InternSettings(
       id: id,
@@ -45,6 +62,9 @@ class InternSettings {
       expectedTimeIn: expectedTimeIn ?? this.expectedTimeIn,
       expectedTimeOut: expectedTimeOut ?? this.expectedTimeOut,
       lunchBreakMins: lunchBreakMins ?? this.lunchBreakMins,
+      officeLat: officeLat ?? this.officeLat,
+      officeLng: officeLng ?? this.officeLng,
+      programType: programType ?? this.programType,
     );
   }
 }
