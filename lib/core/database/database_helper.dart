dfd1619549b2c1f662c6cda689fbf79e4a17ce07
Raver Miradora <ravermiradora0@gmail.com>
2026-04-02 00:14:30 +0800

    chore: migrate sqlite schema to allow lunch break punches

--- lib/core/database/database_helper.dart
+++ lib/core/database/database_helper.dart
@@ -1,10 +1,6 @@
 import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import '../../data/models/time_log.dart';
-import '../../data/models/daily_report.dart';
-import '../../data/models/intern_profile.dart';
-import '../../data/models/intern_settings.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
@@ -24,7 +20,7 @@ class DatabaseHelper {
 
     return await openDatabase(
       path,
-      version: 2,
+      version: 5,
       onCreate: _createDB,
       onUpgrade: _upgradeDB,
     );
@@ -38,6 +34,32 @@ class DatabaseHelper {
         // Ignore if column already exists
       }
     }
+    if (oldVersion < 3) {
+      try {
+        await db.execute('ALTER TABLE intern_settings ADD COLUMN office_lat REAL');
+        await db.execute('ALTER TABLE intern_settings ADD COLUMN office_lng REAL');
+        await db.execute('ALTER TABLE intern_settings ADD COLUMN program_type TEXT DEFAULT "OJT"');
+      } catch (e) {
+        // Ignore if columns already exist
+      }
+    }
+    if (oldVersion < 4) {
+      try {
+        await db.execute('ALTER TABLE time_logs ADD COLUMN is_fieldwork INTEGER DEFAULT 0');
+        await db.execute('ALTER TABLE time_logs ADD COLUMN status TEXT DEFAULT "WORK"');
+        await db.execute('ALTER TABLE time_logs ADD COLUMN remarks TEXT');
+      } catch (e) {
+        // Ignore if columns already exist
+      }
+    }
+    if (oldVersion < 5) {
+      try {
+        await db.execute('ALTER TABLE time_logs ADD COLUMN time_lunch_out TEXT');
+        await db.execute('ALTER TABLE time_logs ADD COLUMN time_lunch_in TEXT');
+      } catch (e) {
+        // Ignore if columns already exist
+      }
+    }
   }
 
   Future _createDB(Database db, int version) async {
@@ -46,15 +68,20 @@ class DatabaseHelper {
       CREATE TABLE time_logs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         date TEXT NOT NULL,
-        time_in TEXT NOT NULL,
+        time_in TEXT,
+        time_lunch_out TEXT,
+        time_lunch_in TEXT,
         time_out TEXT,
-        latitude_in REAL NOT NULL,
-        longitude_in REAL NOT NULL,
+        latitude_in REAL,
+        longitude_in REAL,
         latitude_out REAL,
         longitude_out REAL,
-        photo_path_in TEXT NOT NULL,
+        photo_path_in TEXT,
         photo_path_out TEXT,
-        sync_status INTEGER NOT NULL DEFAULT 0
+        sync_status INTEGER NOT NULL DEFAULT 0,
+        is_fieldwork INTEGER NOT NULL DEFAULT 0,
+        status TEXT NOT NULL DEFAULT "WORK",
+        remarks TEXT
       )
     ''');
 
@@ -85,7 +112,10 @@ class DatabaseHelper {
         target_hours INTEGER NOT NULL,
         expected_time_in TEXT NOT NULL,
         expected_time_out TEXT NOT NULL,
-        lunch_break_mins INTEGER NOT NULL
+        lunch_break_mins INTEGER NOT NULL,
+        office_lat REAL,
+        office_lng REAL,
+        program_type TEXT NOT NULL DEFAULT "OJT"
       )
     ''');
 
@@ -96,6 +126,7 @@ class DatabaseHelper {
       'expected_time_in': '08:00',
       'expected_time_out': '17:00',
       'lunch_break_mins': 60,
+      'program_type': 'OJT',
     });
     
     // Insert Default Profile Setup
