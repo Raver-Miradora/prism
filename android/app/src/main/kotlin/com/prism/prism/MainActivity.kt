d6142c2357ec6e043967d4412bf41b9ed597c0d0
Raver Miradora <ravermiradora0@gmail.com>
2026-04-08 22:05:05 +0800

    android: update MainActivity for native plugin bridging

--- android/app/src/main/kotlin/com/prism/prism/MainActivity.kt
+++ android/app/src/main/kotlin/com/prism/prism/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.prism.prism
 
 import android.provider.Settings
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val CHANNEL = "com.prism.security/settings"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
